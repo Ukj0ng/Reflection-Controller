62d15884e6754bc82e89a22880f1e1c4a2d56f4f
Ukjong <xb253q@gmail.com>
2024-05-04 17:31:18 +0900

    feat: 요청을 처리하기 위한 함수 생성 - join - login - user - hello

--- src/main/java/com/uk/reflection/controller/UserController.java
+++ src/main/java/com/uk/reflection/controller/UserController.java
@@ -1,2 +1,26 @@
-package com.uk.reflection.controller;public class UserController {
+package com.uk.reflection.controller;
+
+import com.uk.reflection.anno.RequestMapping;
+
+public class UserController {
+
+    @RequestMapping("/join")
+    public String join() {
+        return "join.html";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login.html";
+    }
+
+    @RequestMapping("/user")
+    public String user() {
+        return "user.html";
+    }
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello.html";
+    }
 }
