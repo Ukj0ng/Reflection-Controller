b88d2f0675ce05bd99d27590e8a503c0e5bae4ff
Ukjong <xb253q@gmail.com>
2024-05-05 15:24:03 +0900

    feat: join시 사용할 dto

--- src/main/java/com/uk/reflection/controller/dto/JoinDto.java
+++ src/main/java/com/uk/reflection/controller/dto/JoinDto.java
@@ -1,2 +1,10 @@
-package com.uk.reflection.controller.dto;public class JoinDto {
+package com.uk.reflection.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class JoinDto {
+    private String username;
+    private String password;
+    private String email;
 }
