f3d46068c1bd781edddb87548270e4c96bc6c95e
Ukjong <xb253q@gmail.com>
2024-05-05 15:26:03 +0900

    test: key값을 setter로 바꾸는 테스트

--- src/test/java/com/uk/reflection/filter/DispatcherTests.java
+++ src/test/java/com/uk/reflection/filter/DispatcherTests.java
@@ -1,2 +1,15 @@
-package com.uk.reflection.filter;public class DispatcherTests {
+package com.uk.reflection.filter;
+
+import org.junit.jupiter.api.Test;
+
+public class DispatcherTests {
+    @Test
+    public void 키값을세터로바꾸기() {
+        String key = "username";
+
+        String firstKey = "set";
+        String upperKey = key.substring(0, 1).toUpperCase() + key.substring(1);
+
+        System.out.println(firstKey + upperKey);
+    }
 }
